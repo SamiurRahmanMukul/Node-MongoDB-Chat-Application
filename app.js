23f0565917bb877672a5bf2b86d1008e563fecf1
Mukul <sr.mukul9090@gmail.com>
2021-10-17 08:53:13 +0600

    real time chat functionality build using socket.io

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // external imports
 const express = require("express");
+const path = require("path");
+const http = require("http");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const path = require("path");
 const cookieParser = require("cookie-parser");
+const moment = require("moment");
 
 // internal imports
 const { notFoundHandler, errorHandler } = require("./src/middleware/common/errorHandler");
@@ -12,7 +14,15 @@ const usersRouter = require("./src/router/usersRouter");
 const inboxRouter = require("./src/router/inboxRouter");
 
 const app = express();
+const server = http.createServer(app);
 dotenv.config();
+
+// socket creation
+const io = require("socket.io")(server);
+global.io = io;
+
+// set comment as app locals
+app.locals.moment = moment;
 
 // database connection
 mongoose
--- src/router/inboxRouter.js
+++ src/router/inboxRouter.js
@@ -2,13 +2,26 @@
 const express = require("express");
 
 // internal imports
-const { getInbox } = require("./../controller/inboxController");
+const { getInbox, searchUser, addConversation, getMessages, sendMessage } = require("./../controller/inboxController");
 const decorateHtmlResponse = require("./../middleware/common/decorateHtmlResponse");
 const { checkLogin } = require("./../middleware/common/checkLogin");
+const attachmentUpload = require("./../middleware/inbox/attachmentUpload");
 
 const router = express.Router();
 
-// inbox
+// inbox page
 router.get("/", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
+
+// search user for conversation
+router.post("/search", checkLogin, searchUser);
+
+// add conversation
+router.post("/conversation", checkLogin, addConversation);
+
+// get messages of a conversation
+router.get("/messages/:conversation_id", checkLogin, getMessages);
+
+// send message
+router.post("/message", checkLogin, attachmentUpload, sendMessage);
 
 module.exports = router;
