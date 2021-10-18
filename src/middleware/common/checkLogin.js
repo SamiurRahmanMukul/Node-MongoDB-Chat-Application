46d940f6cc0036e40d00ffba2a4c87d8fbe325da
Mukul <sr.mukul9090@gmail.com>
2021-10-18 12:47:10 +0600

    chat applicaion user role management functionality build

--- src/middleware/common/checkLogin.js
+++ src/middleware/common/checkLogin.js
@@ -1,6 +1,7 @@
-// external imports
 const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
 
+// auth guard to protect routes that need authentication
 const checkLogin = (req, res, next) => {
   let cookies = Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null;
 
@@ -39,6 +40,7 @@ const checkLogin = (req, res, next) => {
   }
 };
 
+// redirect already logged in user to inbox page
 const redirectLoggedIn = function (req, res, next) {
   let cookies = Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null;
 
@@ -49,7 +51,29 @@ const redirectLoggedIn = function (req, res, next) {
   }
 };
 
+// guard to protect routes that need role based authorization
+function requireRole(role) {
+  return function (req, res, next) {
+    if (req.user.role && role.includes(req.user.role)) {
+      next();
+    } else {
+      if (res.locals.html) {
+        next(createError(401, "You are not authorized to access this page!"));
+      } else {
+        res.status(401).json({
+          errors: {
+            common: {
+              msg: "You are not authorized!",
+            },
+          },
+        });
+      }
+    }
+  };
+}
+
 module.exports = {
   checkLogin,
   redirectLoggedIn,
+  requireRole,
 };
--- src/router/usersRouter.js
+++ src/router/usersRouter.js
@@ -1,22 +1,23 @@
 // external imports
 const express = require("express");
+const { check } = require("express-validator");
 
 // internal imports
 const { getUsers, addUser, removeUser } = require("./../controller/usersController");
 const decorateHtmlResponse = require("./../middleware/common/decorateHtmlResponse");
 const avatarUpload = require("./../middleware/users/avatarUpload");
 const { addUserValidators, addUserValidationHandler } = require("./../middleware/users/userValidators");
-const { checkLogin } = require("./../middleware/common/checkLogin");
+const { checkLogin, requireRole } = require("./../middleware/common/checkLogin");
 
 const router = express.Router();
 
-// login page
-router.get("/", decorateHtmlResponse("Users"), checkLogin, getUsers);
+// users page
+router.get("/", decorateHtmlResponse("Users"), checkLogin, requireRole(["admin"]), getUsers);
 
 // add user
-router.post("/", checkLogin, avatarUpload, addUserValidators, addUserValidationHandler, addUser);
+router.post("/", checkLogin, requireRole(["admin"]), avatarUpload, addUserValidators, addUserValidationHandler, addUser);
 
 // remove user
-router.delete("/:id", removeUser);
+router.delete("/:id", checkLogin, requireRole(["admin"]), removeUser);
 
 module.exports = router;
